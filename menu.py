f94dc3667f2c00ad0b5814c03181989f262997b6
Alfonso Domínguez García <55954910+AlfonDG@users.noreply.github.com>
2020-02-17 11:52:28 +0100

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,29 @@
+#Creación menu
+
+menu = True
+
+while menu:
+print("=================================")
+print("Cargar Cartas")
+print("Cargar cartas enemigo")
+print("Cargar mazo aleatorio")
+print("Crear mazo aleatorio")
+print("Crear mazo ofensivo")
+print("Crear mazo equilibrado")
+print("Crear mazo aleatorio Enemigo")
+print("Crear mazo ofensivo Enemigo")
+print("Cargar mazo defensivo Enemigo")
+print("Crear mazo equilibrado Enemigo")
+print("Luchar Jugador vs Jugador")
+print("Luchar Jugador vs Bot (arcade)")
+print("Luchar Jugador vs Bot (liga)")
+print("=================================")
+
+if menu == "Cargar Cartas":
+    print("Cargando Cartas.....")
+elif menu == "Cargar cartas enemigo":
+    print("Generando cartas enemigo")
+elif menu == "Cargar mazo aleatorio":
+    print("Cargando un mazo aleatorio")
+elif menu == "Crear mazo aleatorio":
+    print("Creando mazo ")
